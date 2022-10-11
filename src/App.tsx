ab76d03a1da80589d3e657f00fce0af601f7c472
Rodrigo Merlone <alestori7@gmail.com>
2022-10-11 14:18:59 -0300

    adds logic to open and close dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+import { useState } from "react"
 import "./App.sass"
 import Dialog from "./components/Dialog"
 
 function App() {
+	const [isOpen, setIsOpen] = useState(true)
+
 	return (
 		<main>
 			<h1 className="title">Lemon</h1>
-			<button className="open-dialog">Open Dialog</button>
+			<button className="open-dialog" onClick={() => setIsOpen(true)}>
+				Open Dialog
+			</button>
 			<Dialog
 				title="Dialog with overlay click"
 				description="You are able to close this dialog window by clicking on the overlay, as well as pressing ESC or in the close button"
-        isOpen={true}
+				isOpen={isOpen}
+				onClose={() => setIsOpen(false)}
 			/>
 		</main>
 	)
--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -5,12 +5,16 @@ interface Props {
 	title?: string
 	description?: string
 	isOpen: boolean
+	onClose: () => void
 }
 
-const Dialog = ({ title, description, isOpen }: Props) => {
+const Dialog = ({ title, description, isOpen, onClose }: Props) => {
 	return (
 		<div className="overlay" hidden={!isOpen}>
 			<div className="dialog" hidden={!isOpen}>
+				<button className="close-dialog" onClick={onClose}>
+					&#10005;
+				</button>
 				<h1>{title}</h1>
 				<p>{description}</p>
 			</div>
